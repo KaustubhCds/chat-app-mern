52198877c72b53a0f9527e8480cb043787a8eb3d
KaustubhCds <kphatangare12@gmail.com>
2024-07-21 13:57:41 +0530

    kp

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
 	return (
-		<div className='flex h-screen rounded-lg overflow-hidden bg-gray-100 shadow-lg'>
+		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 			<Sidebar />
 			<MessageContainer />
 		</div>
